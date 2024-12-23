67d4fce96e23025ee1a62b48b551d4f3d2cf69e8
wyb <happycodehouse@gmail.com>
2024-12-23 11:57:34 +0900

    tab-dog product js

--- resources/js/gsap/tab-dog.js
+++ resources/js/gsap/tab-dog.js
@@ -1,13 +1,11 @@
 (function () {
-    // 1. click the remote > li.
-    // 2. change the things: content(bg/object transition delay), circle color.
-
     const remoteBtn = Array.from(document.querySelectorAll(".remote li"));
     const productContent = document.getElementsByClassName("content");
     const contentBg = document.querySelectorAll(".product .bg");
     const contentObject = document.getElementsByClassName("object");
+    let currentTimeline; // 현재 애니메이션 타임라인 저장
 
-    console.log(contentBg);
+    productContent[0].classList.add("active");
 
     remoteBtn.forEach(function (btn, index) {
         btn.addEventListener("click", function () {
@@ -16,20 +14,41 @@
                 b.classList.toggle("active", b === btn);
             });
 
+            // 모든 콘텐츠에서 'active' 클래스를 제거하고 초기 상태로 되돌리기
             Array.from(productContent).forEach(function (content, i) {
-                content.classList.toggle("active", i === index);
+                if (i !== index) {
+                    content.classList.remove("active");
+                    contentBg[i].style.cssText = '';
+                    gsap.set(contentBg[i], { width: 0, height: 0 ,alpha: 0 });
+                    gsap.set(contentObject[i], { transform: "scale(0)" });
+                } else {
+                    content.classList.add("active");
+                }
             });
 
-            const tl = gsap.timeline();
-            tl.to(contentBg[index], {
-                top: 0,
-                left: 0,
-                bottom: 0,
-                right: 0,
-                width: "100%",
-                height: "100%",
-                duration: 1,
-            });
+            // 이전 애니메이션이 존재하면 종료
+            if (currentTimeline) {
+                currentTimeline.kill();
+            }
+
+            currentTimeline = gsap.timeline();
+            currentTimeline
+                .to(contentBg[index], {
+                    top: 0,
+                    left: 0,
+                    bottom: 0,
+                    right: 0,
+                    width: "100%",
+                    height: "100%",
+                    alpha: 0.5,
+                    duration: 1
+                }, "productGSAP")
+                .to(contentObject[index], {
+                    scale: 1,
+                    // rotation: 360,
+                    duration: 0.5,
+                    ease: "power1.out"
+                }, "productGSAP");
         });
     });
 })();
